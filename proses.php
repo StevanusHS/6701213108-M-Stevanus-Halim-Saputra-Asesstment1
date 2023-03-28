97fcdd7ad93eb49b7457ec0791103b9b52374e2e
StevanusHS <steven.putra7902@gmail.com>
2023-03-28 12:07:09 +0700

    v1.2.1

--- proses.php
+++ proses.php
@@ -3,18 +3,21 @@ $angka1 = $_POST['angka1'];
 $angka2 = $_POST['angka2'];
 $operator = $_POST['operator'];
 
-if ($operator == '+') {
-    $hasil = $angka1 + $angka2;
-    echo "Hasil penjumlahan dari $angka1 dan $angka2 adalah $hasil";
-} else if ($operator == '-') {
-    $hasil = $angka1 - $angka2;
-    echo "Hasil pengurangan dari $angka1 dan $angka2 adalah $hasil";
-} else if ($operator == '*') {
-    $hasil = $angka1 * $angka2;
-    echo "Hasil perkalian dari $angka1 dan $angka2 adalah $hasil";
-} else if ($operator == '/') {
-    $hasil = $angka1 / $angka2;
-    echo "Hasil pembagian dari $angka1 dan $angka2 adalah $hasil";
-} else {
-    echo "Operator yang Anda pilih tidak valid";
+switch ($operator) {
+    case '+':
+        $hasil = $angka1 + $angka2;
+        break;
+    case '-':
+        $hasil = $angka1 - $angka2;
+        break;
+    case '*':
+        $hasil = $angka1 * $angka2;
+        break;
+    case '/':
+        $hasil = $angka1 / $angka2;
+        break;
+    default:
+        $hasil = "Operasi tidak valid";
 }
+
+echo "Hasil operasi $angka1 $operator $angka2 adalah $hasil";
